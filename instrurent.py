4bbd9674a4a47747a5033eb3704941140b5f74b2
Milttreonna Owens <mowens@basecampcodingacademy.org>
2017-02-02 15:49:47 -0600

    made test_validEmail_upercase function

--- instrurent.py
+++ instrurent.py
@@ -13,6 +13,10 @@ def test_validEmail():
     assert validEmail("onna@gmail.com") != "Invalid"
 
 
+def test_validEmail_uppercase():
+    assert validEmail("ONNA@GMAIL.COM") == "onna@gmail.com"
+
+
 if __name__ == '__main__':
     # user = input("Customer or Employee?").lower().strip()
     userEmail = input("Enter your email address: ")
