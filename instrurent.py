244d08aee75e7d9b75c89cffa150e7f9418703ec
Milttreonna Owens <mowens@basecampcodingacademy.org>
2017-02-16 11:00:13 -0600

    added validEmail() to core.py

--- instrurent.py
+++ instrurent.py
@@ -1,38 +1,8 @@
 from openedFiles import *
+from core import *
 import sys
 
-#this would be core
-
-
-def validEmail():
-    '''Takes user's email and checks to see if it's valid or not'''
-
-    if "@" and ".com" in userEmail:
-        if userEmail in emails:
-            #adds email to user's info
-            userInfo_lst.append(userEmail)
-            #adds date to user's info
-            date_dict["Date"] = (
-                str(now.month) + "-" + str(now.day) + "-" + str(now.year))
-            userInfo_lst.append(date_dict)
-            return ("Previous user")
-
-        else:
-            print("New user")
-            #writes the user's email to the email file
-            with open("emails.csv", "a") as emailFile:
-                emailFile.write(userEmail + '\n')
-            userInfo_lst.append(userEmail)
-            date_dict["Date"] = (
-                str(now.month) + "-" + str(now.day) + "-" + str(now.year))
-            userInfo_lst.append(date_dict)
-            return ("Valid email")
-    elif userEmail == "q":
-        print("Canceling. . . ")
-        sys.exit()
-    else:
-        print("Invalid email")
-        sys.exit()
+#this would be main
 
 
 def show_inventory():
